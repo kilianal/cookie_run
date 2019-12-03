356a2a8a9d3ed118f9626fb494617f8192bcdb39
Juliane Trapp <trappj@media-saturn.com>
2019-12-03 10:08:02 +0100

    Platzhalter für Score und Bewegungsanweisung hinzugefügt

--- script.js
+++ script.js
@@ -7,13 +7,35 @@ window.onload = function() {
 function startGame() {
 console.log("Game started");
 let cookieGame = new CanvasGame(900,700);
+let interaction = new InteractionManual (200,700);
+let scoreDisplay = new ScoreDisplay(200,700);
 let monster = new MovingMonster(cookieGame.ctx);
 console.log(monster.img);
 monster.draw();
 
-
 }
 
+class InteractionManual {
+    constructor(width,height) {
+        this.canvas = document.createElement("canvas");
+        this.ctx = this.canvas.getContext("2d");
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.canvas.style = "border: 2px solid whitesmoke" ;
+        document.getElementById("interactions").appendChild(this.canvas);
+}
+}
+
+class ScoreDisplay {
+    constructor(width,height) {
+        this.canvas = document.createElement("canvas");
+        this.ctx = this.canvas.getContext("2d");
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.canvas.style = "border: 2px solid whitesmoke" ;
+        document.getElementById("score").appendChild(this.canvas);
+}
+}
 
 class CanvasGame {
     constructor(width,height) {
@@ -25,6 +47,8 @@ class CanvasGame {
         this.canvas.style.backgroundImage = "url('pictures/wintry-2915190_1280.png')";
         this.canvas.style.backgroundRepeat = "no-repeat";
         this.canvas.style.backgroundSize = "900px 700px";
+        this.canvas.style.marginRight = "10px";
+        this.canvas.style.marginLeft = "10px";
         document.getElementById("game-board").appendChild(this.canvas);
         //this.updateCanvas = this.updateCanvas.bind(this);
         //this.intervall = setInterval(this.updateCanvas, 50);
