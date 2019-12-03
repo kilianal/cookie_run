c00d3ba9b449a2f8c7d802d55ef8f2cd9a3b7b8e
Juliane Trapp <trappj@media-saturn.com>
2019-12-03 13:03:44 +0100

    Doppeljump und falsche Endposition bei Jump gefixed. Seitengrenzen bei gehaltener Taste gefixed.

--- script.js
+++ script.js
@@ -1,143 +1,136 @@
 window.onload = function() {
-    document.getElementById("start-game").onclick = function() {
-    startGame();       
-    }
-}
+  document.getElementById("start-game").onclick = function() {
+    startGame();
+  };
+};
 
 function startGame() {
-console.log("Game started");
-let cookieGame = new CanvasGame(900,700);
-let interaction = new InteractionManual (200,700);
-let scoreDisplay = new ScoreDisplay(200,700);
+  console.log("Game started");
+  let cookieGame = new CanvasGame(900, 700);
+  let interaction = new InteractionManual(200, 700);
+  let scoreDisplay = new ScoreDisplay(200, 700);
 }
-
 
 //Beschreibung für den Nutzer wie das Spiel gespielt werden muss
 class InteractionManual {
-    constructor(width,height) {
-        this.canvas = document.createElement("canvas");
-        this.ctx = this.canvas.getContext("2d");
-        this.canvas.width = width;
-        this.canvas.height = height;
-        this.canvas.style = "border: 2px solid whitesmoke" ;
-        document.getElementById("interactions").appendChild(this.canvas);
+  constructor(width, height) {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.canvas.style = "border: 2px solid whitesmoke";
+    document.getElementById("interactions").appendChild(this.canvas);
+  }
 }
-}
-
 
 //Anzeige der gesammelten Punkte
 class ScoreDisplay {
-    constructor(width,height) {
-        this.canvas = document.createElement("canvas");
-        this.ctx = this.canvas.getContext("2d");
-        this.canvas.width = width;
-        this.canvas.height = height;
-        this.canvas.style = "border: 2px solid whitesmoke" ;
-        document.getElementById("score").appendChild(this.canvas);
+  constructor(width, height) {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.canvas.style = "border: 2px solid whitesmoke";
+    document.getElementById("score").appendChild(this.canvas);
+  }
 }
-}
-
 
 //Hier fangen die Klassen für das Cookiemonsterspiel an
 class CanvasGame {
-    constructor(width,height) {
-        this.canvas = document.createElement("canvas");
-        this.ctx = this.canvas.getContext("2d");
-        this.canvas.width = width;
-        this.canvas.height = height;
-        this.canvas.style = "border: inset 12px whitesmoke" ;
-        this.canvas.style.backgroundImage = "url('pictures/wintry-2915190_1280.png')";
-        this.canvas.style.backgroundRepeat = "no-repeat";
-        this.canvas.style.backgroundSize = "900px 700px";
-        this.canvas.style.marginRight = "10px";
-        this.canvas.style.marginLeft = "10px";
-        document.getElementById("game-board").appendChild(this.canvas);
-        this.updateGameState = this.updateGameState.bind(this);
-        this.intervall = setInterval(this.updateGameState, 30);
-        this.monster = new MovingMonster(this.ctx);
-        
-    }
-    updateGameState() {
-        
-        this.clearCanvas();
-        this.monster.updateMonster();
-        this.monster.draw(); 
-    }
-    clearCanvas(){
-        this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
-}
+  constructor(width, height) {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.canvas.style = "border: inset 12px whitesmoke";
+    this.canvas.style.backgroundImage =
+      "url('pictures/wintry-2915190_1280.png')";
+    this.canvas.style.backgroundRepeat = "no-repeat";
+    this.canvas.style.backgroundSize = "900px 700px";
+    this.canvas.style.marginRight = "10px";
+    this.canvas.style.marginLeft = "10px";
+    document.getElementById("game-board").appendChild(this.canvas);
+    this.updateGameState = this.updateGameState.bind(this);
+    this.intervall = setInterval(this.updateGameState, 30);
+    this.monster = new MovingMonster(this.ctx);
+  }
+  updateGameState() {
+    this.clearCanvas();
+    this.monster.updateMonster();
+    this.monster.draw();
+  }
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
 }
 
 //Klasse für die bewegenden Objekte
 class MovingObjects {
-    constructor(xPosition, yPosition, ctx, width, height) {
-        this.ctx = ctx;
-        this.height = height;
-        this.width = width;
-        this.xPosition = xPosition;
-        this.yPosition = yPosition;
-        this.ySpeed = 0;
-        this.xSpeed = 0;
-      
+  constructor(xPosition, yPosition, ctx, width, height) {
+    this.ctx = ctx;
+    this.height = height;
+    this.width = width;
+    this.xPosition = xPosition;
+    this.yPosition = yPosition;
+    this.ySpeed = 0;
+    this.xSpeed = 0;
+  }
 }
-
-
-}
-
 
 //Klasse für den Spieler, der sich bewegt
 class MovingMonster extends MovingObjects {
-    constructor( ctx ){
-        super(450, 570, ctx, 80, 90);
-        this.img = new Image();
-        this.img.src = "pictures/cookiemonster_player.png";
-        document.onkeydown = e => {
-             switch (e.keyCode) {
-              case 37:
-                this.xSpeed -= 3;
-                break;
-              case 39:
-              this.xSpeed += 3;
-                break;
-              case 32:
-              this.ySpeed-=15; 
-              break;
-              default:
-            }
-          };
-          document.onkeyup = e => {
-            this.xSpeed = 0;
-            //this.ySpeed=15;
+  constructor(ctx) {
+    super(450, 570, ctx, 80, 90);
+    this.img = new Image();
+    this.img.src = "pictures/cookiemonster_player.png";
+    document.onkeydown = e => {
+      switch (e.keyCode) {
+        case 37:
+          this.xSpeed -= 9;
+          break;
+        case 39:
+          this.xSpeed += 9;
+          break;
+        case 32:
+          this.ySpeed -= 30;
+          break;
+        default:
+      }
+    };
+    document.onkeyup = e => {
+      this.xSpeed = 0;
+      if (this.yPosition < 570) {
+        this.ySpeed = 30;
+      }
+    };
+  }
 
-            
-            
-          };
+  updateMonster() {
+    this.xPosition += this.xSpeed;
+    this.yPosition += this.ySpeed;
+    if (this.yPosition <= 240) {
+      this.ySpeed = 30;
+      this.yPosition += this.ySpeed;
     }
-
-    updateMonster(){
-        this.xPosition+=this.xSpeed;
-        this.yPosition+=this.ySpeed;
-        if(this.yPosition<=285){
-            this.ySpeed=15;
-            this.yPosition+=this.ySpeed
-                       
-        }
-        if(this.yPosition==570){
-            this.yPosition=570;
-            this.ySpeed=0;
-        } 
-        if(this.xPosition<=0){
-           this.xPosition+=10;
-        }  
-        if(this.xPosition>=820){
-            this.xPosition-=10;
-        }
+    if (this.yPosition >= 570) {
+      this.yPosition = 570;
+      this.ySpeed = 0;
     }
+    if (this.xPosition <= 0) {
+      this.xPosition = 5;
+    }
+    if (this.xPosition >= 820) {
+      this.xPosition = 815;
+    }
+  }
 
-    draw (){
-        
-        this.ctx.drawImage(this.img, this.xPosition, this.yPosition, this.width, this.height)
-         
+  draw() {
+    this.ctx.drawImage(
+      this.img,
+      this.xPosition,
+      this.yPosition,
+      this.width,
+      this.height
+    );
+  }
 }
-}
-
