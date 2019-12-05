0efe49298147608994d634e1c5b75a886bb78362
JulianeTrapp <trappj@media-saturn.com>
2019-12-05 01:03:17 +0100

    Win Logik hinzugefügt, Timer auf 60 gesetzt, Lose screen darstellung verbessert

--- script.js
+++ script.js
@@ -7,26 +7,24 @@ window.onload = function () {
   document.getElementById("start-game").onclick = function () {
     startGame();
     points = 0;
-    timer = 100;
-    
+    timer = 60;
   };
 };
- 
+
 function startGame() {
   let scoreDisplay = new ScoreDisplay(300, 700);
-  let interaction = new InteractionManual(300,700);
+  let interaction = new InteractionManual(300, 700);
   let cookieGame = new CanvasGame(900, 700, scoreDisplay);
   let gameBoardCanvas = document.getElementById("game-board");
   let interactionCanvas = document.getElementById("interactions");
   let scoreDisplayCanvas = document.getElementById("score");
   //Blockiert den Start Button um unfreiwilligen Neustart zu verhindern, bis stopGame() ausgeführt wird. 
-  document.getElementById("start-game").setAttribute("disabled","disabled");
+  document.getElementById("start-game").setAttribute("disabled", "disabled");
   if (document.getElementById("game-board").children.length > 0) {
     gameBoardCanvas.removeChild(gameBoardCanvas.childNodes[0]);
     interactionCanvas.removeChild(interactionCanvas.childNodes[0]);
     scoreDisplayCanvas.removeChild(scoreDisplayCanvas.childNodes[0]);
   }
-
 }
 
 
@@ -62,15 +60,15 @@ class CanvasGame {
   updateGameState() {
     this.scoreCanvas.updateScore();
     this.clearCanvas();
-    //Bedinung damit zu einer bestimmten Zeit die Obstacles ins Canvas laufen
+    //Bedingung damit zu einer bestimmten Zeit die Obstacles ins Canvas laufen
     this.frames += 1;
-    
-    let randomFrames=  (Math.floor(Math.random()* 150))+125;
+
+    let randomFrames = (Math.floor(Math.random() * 150)) + 125;
     console.log(randomFrames);
     if (this.frames % randomFrames === 0) {
       this.addFruits();
-
     }
+
     this.gamesObjects.forEach(function (gameObject) {
       gameObject.update();
     });
@@ -78,18 +76,15 @@ class CanvasGame {
     //Bedingung damit die Cookies zu einer bestimmten Zeit runterfallen
     if (this.frames % 50 === 0) {
       this.addCookies();
-
     }
+
     this.gamePoints.forEach(function (gameObject) {
       gameObject.update();
     });
 
     this.checkGameOver();
     this.checkWinner();
-    
-
   }
-
 
   clearCanvas() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -106,10 +101,8 @@ class CanvasGame {
     this.gamePoints.push(this.cookie = new MovingCookies(randomCookie, this.ctx));
   }
 
-
   score() {
     points++;
-    //document.getElementById("points").innerText=`Score ${points}` ;
   }
 
 
@@ -119,12 +112,10 @@ class CanvasGame {
         this.gamePoints.splice(this.gamePoints.indexOf(object), 1)
       }
       return this.monster.isCollidedWith(object);
-
     });
 
     if (catched) this.score();
-
-
+    if (timer === 0) this.stopGame("win");
   }
 
   checkGameOver() {
@@ -132,82 +123,86 @@ class CanvasGame {
       return this.monster.isCollidedWith(object);
     });
 
-    if (crashed) this.stopGame();
-    
-
+    if (crashed) this.stopGame("lose");
   }
 
-  stopGame() {
+  stopGame(status) {
     clearInterval(this.interval);
     //Start Game Button wieder activieren
     document.getElementById("start-game").removeAttribute("disabled");
+
     //clearCanvas, damit Cookiemonster und das Kollisionsobjekt verschwindet
     this.clearCanvas();
-    /*Dieser Teil ändert die Transparenz unseres Hintergrundes indem ein weißes Rectangle mit Transparenzwert 0,7
-        drüber gelegt wird
-    */
-   // this.ctx.fillStyle = "whitesmoke";
-   // this.ctx.globalAlpha = 0.7;
-   // this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    /*Damit die Schrift nicht auch Transparent wird muss hier nochmal die Farbe 
-    und die Transparenz wieder auf 1 gesetzt werden
-    */
-    this.ctx.font = "100px Permanent Marker";
-    this.ctx.fillStyle = "whitesmoke";
-    this.ctx.globalAlpha = 1;
-    this.ctx.fillText("Game Over " , (this.canvas.width / 2 - 230), 300);
-    this.canvas.style.backgroundImage = "url('pictures/bground.png')";
 
+    if (status === "win") {
+      this.ctx.fillStyle = "whitesmoke";
+
+      //Dieser Teil ändert die Transparenz unseres Hintergrundes indem ein weißes Rectangle mit Transparenzwert 0,7 drüber gelegt wird
+      this.ctx.globalAlpha = 0.7;
+      this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.textAlign = "center";
+      this.ctx.font = "100px Permanent Marker";
+      this.ctx.fillStyle = "#ed7d32";
+
+      //Damit die Schrift nicht auch Transparent wird muss hier nochmal die Farbe und die Transparenz wieder auf 1 gesetzt werden
+      this.ctx.globalAlpha = 1;
+      this.ctx.fillText("WIN!", (this.canvas.width / 2), 220);
+      this.ctx.font = "42px Permanent Marker";
+      this.ctx.fillText("A full Monster", (this.canvas.width / 2), 300);
+      this.ctx.fillText("is a happy Monster!", (this.canvas.width / 2), 350);
+    }
+
+    if (status === "lose") {
+      this.ctx.font = "100px Permanent Marker";
+      this.ctx.fillStyle = "#ed7d32";
+      this.ctx.globalAlpha = 1;
+      this.ctx.fillText("Game Over ", (this.canvas.width / 2 - 230), 300);
+      this.canvas.style.backgroundImage = "url('pictures/gameover.jpg')";
+      this.canvas.style.backgroundPosition = "center center";
+      this.canvas.style.backgroundSize = "900px";
+    }
   }
 }
 
 //Beschreibung für den Nutzer wie das Spiel gespielt werden muss
 class InteractionManual {
   constructor(width, height) {
-      this.canvas = document.createElement("canvas");
-      this.ctx = this.canvas.getContext("2d");
-      this.canvas.width = width;
-      this.canvas.height = height;
-      this.canvas.style = "border: 2px solid whitesmoke";
-      document.getElementById("interactions").appendChild(this.canvas);
-  //  let divPlatzhalter=document.createElement("div");
-  //  //divPlatzhalter.style = "border: 2px solid whitesmoke";
-  //  divPlatzhalter.style.width=width;
-  //  divPlatzhalter.style.height=height;    
-  //  document.getElementById("interactions").appendChild(divPlatzhalter);
-    
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.canvas.style = "border: 2px solid whitesmoke";
+    document.getElementById("interactions").appendChild(this.canvas);
   }
 }
 
 //Klasse damit der Score angezeigt wird, der vom Spieler erreicht wurde
 class ScoreDisplay {
   constructor(width, height) {
-      this.canvas = document.createElement("canvas");
-      this.ctx = this.canvas.getContext("2d");
-      this.canvas.width = width;
-      this.canvas.height = height;
-      //this.canvas.style = "border: 2px solid whitesmoke";
-      document.getElementById("score").appendChild(this.canvas);
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    document.getElementById("score").appendChild(this.canvas);
 
-      this.snowflake2 = new Image();
-      this.snowflake2.src = "pictures/snowflake2-white.png";
-          
-      this.frames = 0;
-   
+    this.snowflake2 = new Image();
+    this.snowflake2.src = "pictures/snowflake2-white.png";
+
+    this.frames = 0;
   }
 
-  updateScore(){
+  updateScore() {
     this.clearCanvas();
     this.frames += 1;
     this.ctx.globalAlpha = 1;
     this.ctx.textAlign = "center";
     this.ctx.font = "36px Permanent Marker";
     this.ctx.fillStyle = "whitesmoke";
-    
-    this.ctx.fillText('Score: ' + points ,this.canvas.width/2,100);
+    this.ctx.fillText('Score: ' + points, this.canvas.width / 2, 100);
+
     this.ctx.globalAlpha = 0.4;
-    this.ctx.drawImage(this.snowflake2,-10,180,50,50);
-    this.ctx.drawImage(this.snowflake2,240,360,80,80);
+    this.ctx.drawImage(this.snowflake2, -10, 180, 50, 50);
+    this.ctx.drawImage(this.snowflake2, 240, 360, 80, 80);
 
     this.counter();
   }
@@ -216,20 +211,21 @@ class ScoreDisplay {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
-  counter(){
-     //Timer der die Zeit runterzieht
-     if(this.frames%40==0){
-      timer= timer-1;
-     }
+  counter() {
+    //Timer der die Zeit runterzieht
+    if (this.frames % 40 == 0) {
+      timer = timer - 1;
+    }
+
     this.ctx.globalAlpha = 1;
     this.ctx.textAlign = "center";
     this.ctx.font = "36px Permanent Marker";
     this.ctx.fillStyle = "whitesmoke";
-    
-    this.ctx.fillText('Time left: ' + timer ,this.canvas.width/2,50);
+
+    this.ctx.fillText('Time left: ' + timer, this.canvas.width / 2, 50);
     //Timer der die Zeit runterzieht
   }
-}   
+}
 
 
 //Klasse für die bewegenden Objekte
@@ -257,7 +253,6 @@ class MovingObjects {
   }
 
   isCollidedWith(object) {
-
     if (this === object) return false;
     return !(
       this.bottom() - 10 < object.top() ||
@@ -265,7 +260,6 @@ class MovingObjects {
       this.left() > object.right() ||
       this.right() < object.left()
     );
-
   }
 }
 
@@ -275,6 +269,7 @@ class MovingMonster extends MovingObjects {
     super(450, 570, ctx, 80, 90);
     this.img = new Image();
     this.img.src = "pictures/cookiemonster_player.png";
+
     document.onkeydown = e => {
       switch (e.keyCode) {
         case 37:
@@ -296,6 +291,7 @@ class MovingMonster extends MovingObjects {
       }
     };
   }
+
   //MonsterUpdate Methode lässt Monster springen, nach rechts und links bewegen ohne das Spielfeld zu verlassen
   update() {
     this.xPosition += this.xSpeed;
@@ -336,14 +332,13 @@ class MovingCookies extends MovingObjects {
     this.img = new Image();
     this.img.src = "pictures/cookie1.png";
     this.xPosition = xPosition;
-
-
   }
 
   update() {
     this.yPosition += this.ySpeed;
     this.draw();
   }
+
   draw() {
     this.ctx.drawImage(
       this.img,
@@ -353,7 +348,6 @@ class MovingCookies extends MovingObjects {
       this.height
     );
   }
-
 }
 
 
@@ -363,13 +357,13 @@ class MovingFruits extends MovingObjects {
     this.xSpeed = -6;
     this.img = new Image();
     this.img.src = "pictures/apfel.png";
-    
   }
 
   update() {
     this.xPosition += this.xSpeed;
     this.draw();
   }
+  
   draw() {
     this.ctx.drawImage(
       this.img,
@@ -379,6 +373,5 @@ class MovingFruits extends MovingObjects {
       this.height
     );
   }
-
 }
 
